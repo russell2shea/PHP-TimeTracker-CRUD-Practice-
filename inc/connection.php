50cafc6b21df7957a6f35677953afe333cf10bc0
Russell Shea <russell2shea@gmail.com>
2019-01-06 14:12:56 -0500

    added error handeling for DB connection

--- inc/connection.php
+++ inc/connection.php
@@ -1,5 +1,16 @@
 <?php
 
-$db = new PDO("sqlite:".__DIR__."/database.db");
-var_dump($db);
-//$db = new PDO("mysql:host=localhost;dbname=database;port=3306","root","root");
+/* Try to create an object from the php data class to connect to sqlite database file*/
+try{ 
+	$db = new PDO("sqlite:".__DIR__."/database.db");
+
+	//Tell PDO all error should be seen as an exception
+	$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+} 
+
+/* Throw exception if there is problem connecting. Dispaly error message and end*/
+catch (Exception $e){
+	echo $e->getMessage();
+	exit;
+}
+
