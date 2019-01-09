923307fc355c46c905f16005c98edfbce99ea3b1
Russell Shea <russell2shea@gmail.com>
2019-01-08 20:03:18 -0500

    created task list function

--- inc/functions.php
+++ inc/functions.php
@@ -24,6 +24,29 @@
 
 	}
 
+/* Try to pull all tasks from the database */
+	function get_task_list(){
+
+		//Take all code from the connection.php file and copy it here. Wills how warning not fatal error if something goes wrong
+		include 'connection.php';
+
+		//Join projects and tasks tables to get the project title from all tasks
+		$sql = 'SELECT tasks.*, projects.title as project FROM tasks'
+			. ' JOIN projects ON tasks.project_id = projects.project_id';
+		
+		try{
+			return $db->query($sql);
+		}
+
+		//If can't read them show error message and stop
+		catch(Exception $e){
+			echo "Error!:".$e->getMessage()."</br>";
+			return array();
+
+		}
+
+	}
+
 /* Add a project to the DB */
 	function add_project($title, $category){
 		include 'connection.php';
